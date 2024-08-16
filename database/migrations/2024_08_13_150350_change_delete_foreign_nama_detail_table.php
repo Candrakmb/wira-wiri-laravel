8563fd5a8a0b96c714e187a50e8cdd8d4839abee
candrakmb <candrakmb07@gmail.com>
2024-08-16 14:34:22 +0700

    fix migration 3

--- database/migrations/2024_08_13_150350_change_delete_foreign_nama_detail_table.php
+++ database/migrations/2024_08_13_150350_change_delete_foreign_nama_detail_table.php
@@ -30,7 +30,6 @@ return new class extends Migration
                   ->references('id')
                   ->on('kedais') // Corrected table name
                   ->onDelete('cascade');
-            $table->timestamps();
         });
     }
 
