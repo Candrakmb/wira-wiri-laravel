739cfd5e1e044b91ed570e8cdbeda988ceedb4da
candrakmb <candrakmb07@gmail.com>
2024-08-16 14:37:26 +0700

    fix migration 4

--- database/migrations/2024_08_13_150350_change_delete_foreign_nama_detail_table.php
+++ database/migrations/2024_08_13_150350_change_delete_foreign_nama_detail_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('order_detail_ekstras', function (Blueprint $table) {
-            $table->dropColumn('nama_ekstra');
-        });
+        // Schema::table('order_detail_ekstras', function (Blueprint $table) {
+        //     $table->dropColumn('nama_ekstra');
+        // });
 
         Schema::table('order_destinations', function (Blueprint $table) {
             $table->dropColumn('alamat_detail');
